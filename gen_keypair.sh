78a22cd4b2cdeccd4d90c35f2fa03c9e787e2e1d
smherwig <smherwig@cs.umd.edu>
2019-11-05 10:32:11 -0500

    gen_keypair.sh: change name of key pair.

--- gen_keypair.sh
+++ gen_keypair.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # generate 2048 bit RSA keypair; output private key
-openssl genrsa -out private.pem 2048
+openssl genrsa -out timeserver-private.pem 2048
 
 # export public key
-openssl rsa -in private.pem -outform PEM -pubout -out public.pem
+openssl rsa -in timeserver-private.pem -outform PEM -pubout -out timeserver-public.pem
